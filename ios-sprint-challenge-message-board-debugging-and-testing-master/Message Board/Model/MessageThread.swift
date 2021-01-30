7e040bcf670e15b3066d33eccbce9992c3ccce93
brysonS <brys1saclaus2@gmail.com>
2021-01-29 19:49:48 -0800

    Clone starter project

---
+++ ios-sprint-challenge-message-board-debugging-and-testing-master/Message Board/Model/MessageThread.swift
@@ -0,0 +1,55 @@
+//
+//  MessageThread.swift
+//  Message Board
+//
+//  Created by Spencer Curtis on 8/7/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class MessageThread: Codable, Equatable {
+
+    let title: String
+    var messages: [MessageThread.Message]
+    let identifier: String
+
+    init(title: String, messages: [MessageThread.Message] = [], identifier: String = UUID().uuidString) {
+        self.title = title
+        self.messages = messages
+        self.identifier = identifier
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let title = try container.decode(String.self, forKey: .title)
+        let identifier = try container.decode(String.self, forKey: .identifier)
+        let messagesDictionaries = try container.decodeIfPresent([String: Message].self, forKey: .messages)
+        
+        let messages = messagesDictionaries?.compactMap({ $0.value }) ?? []
+        
+        self.title = title
+        self.identifier = identifier
+        self.messages = messages
+    }
+
+    
+    struct Message: Codable, Equatable {
+        let text: String
+        let sender: String
+        let timestamp: Date
+        
+        init(text: String, sender: String, timestamp: Date = Date()) {
+            self.text = text
+            self.sender = sender
+            self.timestamp = timestamp
+        }
+    }
+    
+    static func ==(lhs: MessageThread, rhs: MessageThread) -> Bool {
+        return lhs.title == rhs.title &&
+            lhs.identifier == rhs.identifier &&
+            lhs.messages == rhs.messages
+    }
+}
---
+++ ios-sprint-challenge-message-board-debugging-and-testing-master/Message BoardUITests/Message_BoardUITests.swift
@@ -0,0 +1,22 @@
+//
+//  Message_BoardUITests.swift
+//  Message BoardUITests
+//
+//  Created by Spencer Curtis on 9/14/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class Message_BoardUITests: XCTestCase {
+    
+    var app = XCUIApplication()
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        
+        // NOTE: Keep this setup as is for UI Testing
+        app.launchArguments = ["UITesting"]
+        app.launch()
+    }
+}
